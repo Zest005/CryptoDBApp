9214e9c6dad118b153066d2f209df1479f60397e
Zest005 <Zest005@gmail.com>
2022-12-21 15:46:25 +0200

    Add class GetRequest

--- Model/Cryptocurr.cs
+++ Model/Cryptocurr.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace CryptoDBApp.Model
 {
-    internal class Cryptocurr
+    public class Cryptocurr
     {
+        public Cryptocurr(string id, string name, int rank, string symbol, double marketCapUsd, double priceUsd, double changePercent24Hr)
+        {
+            Id = id;
+            Name = name;
+            Rank = rank;
+            Symbol = symbol;
+            MarketCapUsd = marketCapUsd;
+            PriceUsd = priceUsd;
+            ChangePercent24Hr = changePercent24Hr;
+        }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Rank { get; set; }
+
+        public string Symbol { get; set; }
+        
+        public double MarketCapUsd { get; set; }
+
+        public double PriceUsd { get; set; }
+        
+        public double ChangePercent24Hr { get; set; }
+
+        public static Cryptocurr CreateCryptocurr()
+        {
+            return new Cryptocurr();
+        }
     }
 }
---
+++ Model/GetRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoDBApp.Model
+{
+    public class GetRequest
+    {
+        public GetRequest(string address)
+        {
+            Address = address;
+        }
+
+        HttpWebRequest Request;
+
+        string Address;
+
+        public string Response { get; set; }
+
+        public void Run()
+        {
+            Request = (HttpWebRequest)WebRequest.Create(Address);
+            Request.Method = "Get";
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)Request.GetResponse();
+                var stream = response.GetResponseStream();
+
+                if (stream != null)
+                    Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception) { }
+        }
+    }
+}
